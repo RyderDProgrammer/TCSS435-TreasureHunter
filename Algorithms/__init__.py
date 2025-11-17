3792206b8ddfdde90503cf231c4df24e07e5e466
Ryder DeBack <77074811+RyderDProgrammer@users.noreply.github.com>
2025-11-17 07:05:34 -0800

    Creating new file structure for better clarity.

---
+++ Algorithms/__init__.py
@@ -0,0 +1,17 @@
+from . import BFS
+from . import DFS
+from . import UCS
+from . import A_Star
+from . import Greedy_BFS
+from . import MiniMax
+from . import Alpha_Beta
+
+__all__ = [
+    'BFS',
+    'DFS',
+    'UCS',
+    'A_Star',
+    'Greedy_BFS',
+    'MiniMax',
+    'Alpha_Beta'
+]
